98d912cace3de40b13ddb0f95c86991f13dffb67
Mohammad Iftekher (Iftu) Ebne Jalal <iftekheriftu@gmail.com>
2026-03-27 12:21:02 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://iftu119.github.io',
-  base: '/iftekheriftu',
+  site: 'https://iftekheriftu.com',
   integrations: [mdx()],
 });
